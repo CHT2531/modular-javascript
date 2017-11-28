69d323e5b3b9004859a3ebd1b23db32b86571cd0
mmantle-hud <m.e.mantle@hud.ac.uk>
2017-11-28 10:37:12 +0000

    basic functions

--- js/app.js
+++ js/app.js
@@ -1,15 +1,32 @@
+var doubleIt=function(num){
+    return num*2;
+}
+var isANumber=function(num){
+        if(num=="" || isNaN(num))
+        {
+            return false;
+        }
+        return true;
+}
+var clickHandler=function(evnt)
+{
+    doCalc();
+}
+var displayFeedback=function(msg){
+    msgDiv.innerHTML=msg;
+}
 function doCalc()
 {
-	var userNum = userNumBox.value.trim();
-	if(userNum=="" || isNaN(userNum))
-	{
-        msgDiv.innerHTML="You need to enter a number";
+    var userNum = userNumBox.value;
+    if(isANumber(userNum))
+    {
+        var doubleNum=doubleIt(userNum);
+        displayFeedback("Double "+userNum+" is "+doubleNum)
+    }else{
+        displayFeedback("You need to enter a number")
         userNumBox.value="";
         userNumBox.focus();
-	}else{
-		var doubleNum=userNum*2
-        msgDiv.innerHTML="Double "+userNum+" is "+doubleNum;
-	}
+    }
 }
 
 var userNumBox = document.querySelector("#userNum");
